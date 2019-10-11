eeb857c1590ff0c1dd38e62c2294aeb361084f53
Etienne Ghashehbaba <admin@aheadbl.com>
2019-10-11 14:44:47 -0700

    add get action for user api

---
+++ controllers/users/users.get.action.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../../models/User.js');
+var passport = require('passport');
+require('../../config/passport')(passport);
+
+router.get('/', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    User.find(function (err, users) {
+      if (err) return next(err);
+      res.json(users);
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'Unauthorized.'});
+  }
+});
+
+router.get('/:id', function(req, res, next) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) return next(err);
+    res.json(user);
+  });
+});
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+module.exports = router;
